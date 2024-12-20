1b185a75ab53b206bf6e3af9f5eec188d9d3511a
Youngben1 <benjaminrenny@gmail.com>
2024-12-20 11:47:39 +0100

    search section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or angwa" class="py-2 font-bold px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
+    </div>
   </main>
 </template>
+
+<script setup>
+import {ref} from "vue" 
+import axios from "axios"
+
+
+  const mapboxAPIKey = "VUE_APP_ENV_VARIABLE"
+  const searchQuery = ref("");
+  const queryTimeout = ref(null);
+  const mapboxSearchResults = ref(null);
+
+  const getSearchResults = () => {
+
+    clearTimeout(queryTimeout.value);
+    queryTimeout.value = setTimeout(async () => {
+      if(searchQuery !== "") {
+      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`);
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value)
+        return;
+      }
+      mapboxSearchResults.value = null;
+    }, 300);
+  }
+</script>
