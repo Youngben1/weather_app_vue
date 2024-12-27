f4e9bc7c811b682b0355c521c86bba624c784ea8
Youngben1 <benjaminrenny@gmail.com>
2024-12-27 16:12:16 +0100

    cityview loading

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+	try {
+		const weatherData = await axios.get(
+			`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=ef6ad191b597ca3b2ea40c5a737f098f&units=imperial`
+		);
+		// cal current date & time
+		const localOffset = new Date().getTimezoneOffset() * 60000;
+		const utc = weatherData.data.current.dt * 1000 + localOffset;
+		weatherData.data.currentTime =
+			utc + 1000 * weatherData.data.timezone_offset;
+
+		// cal hourly weather offset
+		weatherData.data.hourly.forEach((hour) => {
+			const utc = hour.dt * 1000 + localOffset;
+			hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+		});
+
+		return weatherData;
+	} catch (err) {
+		console.log(err);
+	}
+};
+const weatherData = await getWeatherData();
+</script>
